2ef189661abb7ddc6fff78865b9fd8f32b25327b
SunJet Liu <sunjetliu@gmail.com>
2020-09-04 04:06:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,11 +6,10 @@ def find_even_values(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      # if src[row_index][element_index].even?
+      if src[row_index][element_index].even?
       p src[row_index][element_index]
-      # end
+      end
       element_index += 1
-puts "hello"
     end
     row_index += 1
   end #=> nil
